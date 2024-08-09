cfb29037a61893c5eaeaa6ef48daf7e112d4c1e0
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2024-08-09 00:52:56 -0600

    First test of Java Spring Boot Rest API

---
+++ src/main/java/com/melizalde_ds/springboot_test/java_api/JavaApiApplication.java
@@ -0,0 +1,14 @@
+package com.melizalde_ds.springboot_test.java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/melizalde_ds/springboot_test/java_api/JavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melizalde_ds.springboot_test.java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
